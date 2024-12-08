1bfb067d5c36ffc521daf9fe39e11ebf8079eec1
daniloaraujosp <danilo.araujo.sp@gmail.com>
2024-12-08 09:52:51 -0300

    criando calculadora

--- desafio01-calculadora/src/App.js
+++ desafio01-calculadora/src/App.js
@@ -1,5 +1,7 @@
 import Input from './components/Input';
 import Button from './components/Button';
+import ButtonPurple from './components/ButtonPurple'
+import '@fontsource/roboto/500.css';
 
 import { Container, Content, Row } from './styles';
 import { useState } from 'react';
@@ -18,11 +20,11 @@ const App = () => {
   };
 
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${prev == '0' ? '' : prev}${num}`)
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
   };
 
   const handleSumNumbers = () => {
-    if (firstNumber == '0') {
+    if (firstNumber === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0');
       setOperation('+')
@@ -36,7 +38,7 @@ const App = () => {
   }
 
   const handleMinusNumbers = () => {
-    if (firstNumber == '0') {
+    if (firstNumber === '0') {
       setFirstNumber(String(currentNumber));
       setCurrentNumber('0');
       setOperation('-')
@@ -49,14 +51,68 @@ const App = () => {
        
   }
 
+  const handleMultNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('x')
+    }
+    else {
+      const sum = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+       
+  }
+
+  const handleDivNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/')
+    }
+    else {
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+       
+  }
+
+  const handleQuaNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('√')
+    }
+    else {
+      const sum = Math.sqrt(Number(currentNumber)); 
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+  }
+
   const handleEquals = () => {
-    if (firstNumber != '0' && operation != '' && currentNumber != '0') {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
       switch (operation) {
         case '+':
           handleSumNumbers();
           break;
+        
         case '-':
           handleMinusNumbers();
+          break;  
+        
+        case '/':
+          handleDivNumbers();
+          break; 
+        
+        case '√':
+          handleQuaNumbers();
+          break;
+        
+        case 'x':
+          handleMultNumbers();
           break;
           default:
           break;
@@ -69,28 +125,36 @@ const App = () => {
       <Content>
         <Input value={currentNumber} />
         <Row>
-          <Button label="x" onClick={() => handleAddNumber('x')}/>
-          <Button label="/" onClick={() => handleAddNumber('/')}/>
+          <Button label="→" onClick={handleOnClear}/>
           <Button label="C" onClick={handleOnClear}/>
-          <Button label="X" onClick={() => handleAddNumber('X')}/>
+          <Button label="√" onClick={handleQuaNumbers}/>          
+          <ButtonPurple label="/" onClick={handleDivNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <ButtonPurple label="x" onClick={handleMultNumbers} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <ButtonPurple label="-" onClick={handleMinusNumbers}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
-          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <ButtonPurple label="+" onClick={handleSumNumbers}/>
+          
+        </Row>
+        <Row>
+          <Button label="00" onClick={() => handleAddNumber('00')} />
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="." onClick={() => handleAddNumber('.')} />
           <Button label="=" onClick={handleEquals}/>
+          
         </Row>
       </Content>
     </Container>
--- desafio01-calculadora/src/components/Input/styles.js
+++ desafio01-calculadora/src/components/Input/styles.js
@@ -3,25 +3,27 @@ import styled from 'styled-components';
 export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
-    background-color: #AAAAFF;
+    background-color: #FFFFFF;
 
     display: flex;
     align-items: center;
     justify-content: flex-end;
 
-    font-size: 24px;
+    font-size: 64px;
     font-family: 'Roboto';
     
 
     input{
         width: 100%;
         height: 75px;
-        background-color: #AAAAFF;
+        background-color: #FFFFFF;
         border: 0;
         padding: 0 10px;
 
-        font-size: 24px;
+        font-size: 64px;
         font-family: 'Roboto';
-        color: #FFFFFF
+        color: #292929;
+        font-weight: 500;
+        text-align: right;
     }
 `
